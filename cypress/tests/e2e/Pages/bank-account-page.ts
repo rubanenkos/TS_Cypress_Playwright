9e58153386f8287de8e9c733915517f730092472
Sergey Rubanenko <srubanenko@aligntech.com>
2022-09-12 13:28:41 +0300

    Clear comments

--- cypress/tests/e2e/Pages/bank-account-page.ts
+++ cypress/tests/e2e/Pages/bank-account-page.ts
@@ -11,17 +11,14 @@ class BankAccountPage {
     bankNameInput() {
         return cy.getBySel("bankaccount-bankName-input")
     }
-    // cy.getBySel("bankaccount-bankName-input").should("be.visible").type(newAccount.bankName)
 
     routingNumberInput() {
         return cy.getBySel("bankaccount-routingNumber-input")
     }
-    // cy.getBySel("bankaccount-routingNumber-input").should("be.visible").type(newAccount.routingNumber)
 
     accountNumber() {
         return cy.getBySel("bankaccount-accountNumber-input")
     }
-    // cy.getBySel("bankaccount-accountNumber-input").should("be.visible").type(newAccount.accountNumber)
-    // cy.getBySel("bankaccount-submit").should("be.visible").click()
+
 }
 export default BankAccountPage
